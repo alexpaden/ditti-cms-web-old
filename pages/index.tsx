210000e8ce00f8d6472c42bd6e98ee77583f3bfe
alex paden <padenalex0@gmail.com>
2023-03-07 11:28:31 -0600

    update follow -> index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
-import type { NextPage } from "next";
+import { useState } from "react";
+import { getSession, GetSessionParams } from "next-auth/react";
+import FollowData from "../components/FollowData";
 
-const Home: NextPage = () => {
-  return <div>Test Home Here</div>;
+const FollowPage = ({ fid }: { fid: number }) => {
+  const [sessionFid, setSessionFid] = useState<number | null>(fid);
+
+  return (
+    <div>
+      <h1>Follow Change Tracker</h1>
+      {sessionFid && <FollowData sessionFid={sessionFid} />}
+    </div>
+  );
 };
 
-export default Home;
+export async function getServerSideProps(
+  context: GetSessionParams | undefined
+) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return { props: {} }; // return an empty props object if there is no session
+  }
+
+  return {
+    props: {
+      fid: session.fid,
+    },
+  };
+}
+
+export default FollowPage;
